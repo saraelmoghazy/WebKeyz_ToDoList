bc685ed0758d65caf0224afa2d4c229c7b9778ac
AlShaymaa1 <shaymaa908@yahoo.com>
2019-08-25 13:02:17 +0200

    editing

--- app/src/main/java/com/webkeyz/todo/presentation/BaseFragment.java
+++ app/src/main/java/com/webkeyz/todo/presentation/BaseFragment.java
@@ -1,25 +1,51 @@
 package com.webkeyz.todo.presentation;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+
+import com.webkeyz.todo.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.supercharge.shimmerlayout.ShimmerLayout;
 
 public abstract class BaseFragment<V extends BaseViewModel> extends Fragment {
 
     private V viewModel;
     private ProgressDialog progressDialog;
+    private AlertDialog alertDialog;
+
+    @BindView(R.id.shimmer_layout)
+    ShimmerLayout shimmerLayout;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         viewModel = getViewModel();
         progressDialog = new ProgressDialog(getActivity());
+
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_base, container, false);
+        ButterKnife.bind(this, view);
         observeLoadingIndicator();
         observeHasError();
-
+        observeNetworkConnection();
+        return view;
     }
 
     public abstract V getViewModel();
@@ -27,21 +53,46 @@ public abstract class BaseFragment<V extends BaseViewModel> extends Fragment {
     private void observeLoadingIndicator() {
         viewModel.isLoading.observe(this, isLoading -> {
             if (isLoading) {
-                Toast.makeText(getActivity(), "show ", Toast.LENGTH_SHORT).show();
+                shimmerLayout.startShimmerAnimation();
             } else {
-                Toast.makeText(getActivity(), "hide ", Toast.LENGTH_SHORT).show();
-
+                shimmerLayout.stopShimmerAnimation();
             }
         });
     }
 
 
-    private void observeHasError() {
+    private void observeNetworkConnection() {
         viewModel.hasNetworkError.observe(this, hasError -> {
             if (hasError) {
-                Toast.makeText(getActivity(), "has error ", Toast.LENGTH_SHORT).show();
+                setDialogue();
             }
         });
     }
 
+    private void observeHasError() {
+        viewModel.hasRequestError.observe(this, hasError -> {
+            if (hasError) {
+                Toast.makeText(getActivity(), getActivity().getString(R.string.msg_failure), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setDialogue() {
+        alertDialog = new AlertDialog.Builder(getActivity())
+                .setMessage(getActivity().getResources().getString(R.string.msg_internet_connection))
+                .setPositiveButton(getActivity().getString(R.string.retry), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Continue with delete operation
+                    }
+                })
+                .show();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (alertDialog != null) {
+            alertDialog.dismiss();
+        }
+    }
 }
